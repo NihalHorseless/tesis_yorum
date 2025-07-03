4f092566f0e79a29543b4e09de767cf5a3d4a548
NihalHorseless <onuraslanfb07@hotmail.com>
2025-07-03 13:39:38 +0300

    İlk Commit

---
+++ src/main/java/org/example/tesis_yorum/TesisYorumApplication.java
@@ -0,0 +1,13 @@
+package org.example.tesis_yorum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisYorumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesisYorumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tesis_yorum/TesisYorumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tesis_yorum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisYorumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
